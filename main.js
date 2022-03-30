ab01ee706cb39eb85886481166428c732e2cc12e
Yuri Reis <yuri.reis@itsstecnologia.com.br>
2022-03-30 20:50:31 -0300

    Otimizando código com for

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ function tocaSom(idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
@@ -15,7 +13,5 @@ while (contador < listaDeTeclas.length){
     tecla.onclick = function(){
         tocaSom(idAudio);
     }
-
-    contador = contador + 1;
 
 }
