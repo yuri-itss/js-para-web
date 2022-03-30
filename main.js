758bc590aa529f94020d8793f133dd4091c987d8
Yuri Reis <yuri.reis@itsstecnologia.com.br>
2022-03-30 20:44:36 -0300

    Criando o laço de repetição para percorrer pelos sons

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla_pom');
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+}
